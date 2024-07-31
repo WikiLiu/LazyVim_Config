071d04a98048b6d74d1a2b3320d205fc0858f89f
IU <68397119+WikiLiu@users.noreply.github.com>
2024-07-31 16:36:33 +0800

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -41,6 +41,7 @@ return {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = { width = 20 },
     config = true,
   },
 
